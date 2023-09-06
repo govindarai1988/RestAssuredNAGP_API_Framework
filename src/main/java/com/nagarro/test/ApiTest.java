399cd995a94897aed6a33cd0284be9bec6b2e963
govindarai <govinda.rai@gmail.com>
2023-09-06 19:18:39 +0530

    added log4j

--- src/main/java/com/nagarro/test/ApiTest.java
+++ src/main/java/com/nagarro/test/ApiTest.java
@@ -14,6 +14,7 @@ import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.greaterThan;
 import static org.hamcrest.Matchers.is;
 
+
 class ApiTest extends BaseTest {
     BookingRequestPayload createBookingRequestPayload() {
         SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd");
--- src/main/java/com/nagarro/test/BaseTest.java
+++ src/main/java/com/nagarro/test/BaseTest.java
@@ -12,16 +12,25 @@ import static org.apache.http.HttpStatus.SC_CREATED;
 import static org.apache.http.HttpStatus.SC_OK;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import  org.apache.logging.log4j.LogManager;
+import  org.apache.logging.log4j.Logger;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class BaseTest {
     protected final Faker faker = new Faker();
     protected String token;
+    private static final Logger logger = LogManager.getLogger(BaseTest.class);
 
     @BeforeAll
     void testHealthCheckReturns201() {
         Response response = PingApi.healthCheck();
         assertThat(response.statusCode(), equalTo(SC_CREATED));
+        logger.trace("We've just greeted the user!");
+        logger.debug("We've just greeted the user!");
+        logger.info("We've just greeted the user!");
+        logger.warn("We've just greeted the user!");
+        logger.error("We've just greeted the user!");
+        logger.fatal("We've just greeted the user!");
     }
 
     @BeforeEach
@@ -33,5 +42,11 @@ class BaseTest {
         token = response.as(AuthResponsePayload.class).getToken();
 
         assertThat(response.statusCode(), equalTo(SC_OK));
+        logger.trace("We've just greeted the user!");
+        logger.debug("We've just greeted the user!");
+        logger.info("We've just greeted the user!");
+        logger.warn("We've just greeted the user!");
+        logger.error("We've just greeted the user!");
+        logger.fatal("We've just greeted the user!");
     }
 }
